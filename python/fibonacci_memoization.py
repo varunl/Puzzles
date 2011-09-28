c9a93fe1954aa6c9bb69fdec87ebc1e3f448964e
Varun Loiwal <varunloiwal@gmail.com>
2011-09-27 20:03:24 -0400

    Fibonacci series. Recursive with memoization.

---
+++ python/fibonacci_memoization.py
@@ -0,0 +1,23 @@
+import sys
+
+def main():
+	print "The fibonacci number " + sys.argv[1] + " is",
+	d = {}
+	print fibonacci(int(sys.argv[1]), d)
+
+
+def fibonacci(n, d):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	if d.has_key(n): 
+		return d[n]
+	else:
+		k = fibonacci(n-1, d) + fibonacci(n-2, d)
+		d[n] = k
+		return k
+
+
+main()
+
