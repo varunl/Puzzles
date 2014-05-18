39a650abc7d38464fdae08e8751f76933749255d
varunl <varunloiwal@gmail.com>
2014-05-18 10:36:45 -0700

    Change tabs to spaces.

--- python/fibonacci_memoization.py
+++ python/fibonacci_memoization.py
@@ -1,23 +1,24 @@
 import sys
 
 def main():
-	print "The fibonacci number " + sys.argv[1] + " is",
-	d = {}
-	print fibonacci(int(sys.argv[1]), d)
+    print "The fibonacci number " + sys.argv[1] + " is",
+    d = {}
+    print fibonacci(int(sys.argv[1]), d)
 
 
 def fibonacci(n, d):
-	if n == 0:
-		return 0
-	if n == 1:
-		return 1
-	if d.has_key(n): 
-		return d[n]
-	else:
-		k = fibonacci(n-1, d) + fibonacci(n-2, d)
-		d[n] = k
-		return k
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if d.has_key(n):
+        return d[n]
+    else:
+        k = fibonacci(n-1, d) + fibonacci(n-2, d)
+        d[n] = k
+        return k
 
 
-main()
+if __name__ == "__main__":
+    main()
 
